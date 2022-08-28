9a97de9615dec6af15efa21e5c1e311742ca9eb6
Paweł Habrzyk <pawelhabrzyk@gmail.com>
2022-08-28 16:32:57 +0200

    better state control

--- bluetooth_talker/bluetooth_talker/bluetooth_talker_node.py
+++ bluetooth_talker/bluetooth_talker/bluetooth_talker_node.py
@@ -22,41 +22,44 @@ class BluetoothTalker(Node):
     """Node that controlls pace of scaning and puts individual scans together."""
     def __init__(self):
         super().__init__('bluetooth_talker')
-        self.bd = BlueDot()
+        self.recording = False
         self._action_start_recording_client = ActionClient(self, StartScan, 'start_scan_action_server')
-        while not self.bd.is_pressed:
-            pass
-        self.start_recording()
     
     def start_recording(self):
-        goal_msg = StartScan.Goal()
-        goal_msg.request = 1
-        self._action_start_recording_client.wait_for_server()
-        return self._action_start_recording_client.send_goal_async(goal_msg)
+        if not self.recording:
+            self.recording = True
+            goal_msg = StartScan.Goal()
+            goal_msg.request = 1
+            self._action_start_recording_client.wait_for_server()
+            return self._action_start_recording_client.send_goal_async(goal_msg)
 
     def goal_response_callback(self, future):
         goal_handle = future.result()
         if not goal_handle.accepted:
             self.get_logger().info('Goal rejected :(')
+            self.recording = False
             return
-
+        self.recording = True
         self.get_logger().info('Goal accepted :)')
-
         self._get_result_future = goal_handle.get_result_async()
         self._get_result_future.add_done_callback(self.get_result_callback)
 
     def get_result_callback(self, future):
         result = future.result().result
         self.get_logger().info('Result: {0}'.format(result.sequence))
+        self.recording = False
         rclpy.shutdown()
 
     def feedback_callback(self, feedback_msg):
+        self.recording = True
         feedback = feedback_msg.feedback
         self.get_logger().info('Received feedback: {0}'.format(feedback.partial_sequence))
 
 def main(args=None):
     rclpy.init(args=args)
+    bd = BlueDot()
     bluetooth_talker = BluetoothTalker()
+    bd.when_pressed = bluetooth_talker.start_recording()
     try:
         rclpy.spin(bluetooth_talker)
     except KeyboardInterrupt:
